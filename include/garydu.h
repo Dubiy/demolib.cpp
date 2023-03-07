123c7f9bc36cc1f6baa2fd24647cd3368325acc5
Ihor Dubii <ihor.dubii@bladelabs.io>
2023-03-07 14:40:28 +0000

    init commit

---
+++ include/garydu.h
@@ -0,0 +1,12 @@
+#ifndef GARYDU_H
+#define GARYDU_H
+
+namespace Gary
+{
+    namespace Du
+    {
+        void sayHello();
+    }
+}
+
+#endif // GARYDU_H
---
+++ src/garydu.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// #include "things.h"
+
+namespace Gary
+{
+    namespace Du
+    {
+        void sayHello()
+        {
+            std::cout << "hello, Gary" << std::endl;
+        }
+    }
+}
